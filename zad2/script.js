7afcf795592131e1d78d702418ba121c462cc554
Lnkvch3 <308267@stud.umk.pl>
2024-11-21 09:47:04 +0100

    dodanie pliku

--- zad2/script.js
+++ zad2/script.js
@@ -1,3 +1,17 @@
-let value = 0;
-totalValue += value;
-dodaj.getElementById('totalValue').textContent = totalValue;
+const liczbaElement = document.getElementById('liczba');
+const dodajButton = document.getElementById('dodaj');
+const przelaczKolorButton = document.getElementById('przelaczKolor');
+
+let liczba = 0;
+let czyCzarnyKolor = true;
+
+dodajButton.addEventListener('click', () => {
+    liczba++;
+    liczbaElement.textContent = liczba;
+});
+
+
+przelaczKolorButton.addEventListener('click', () => {
+    czyCzarnyKolor = !czyCzarnyKolor; 
+    liczbaElement.style.color = czyCzarnyKolor ? 'black' : 'red';
+});
